112358fd3fd4ec640c485a83a77b3eddf4cdfdd2
ElaDeCode <eladecode+github@gmail.com>
2024-08-13 23:37:30 +0300

    test ignore build files

--- build.windows/sade/CMakeFiles/myLeaf.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for myLeaf.
-# This may be replaced when dependencies are built.
--- build/resources/shaders/test.fs
+++
@@ -1,9 +0,0 @@
-#version 450 core
-
-out vec4 FragColor;
-in vec3 fragPos;
-in vec3 vertPos;
-
-void main() {
-  FragColor = vec4(vertPos / 50,1);
-}
